e7241644d0638d8bc78c4a2274210c9a477495ed
Mary <okekeifeoma41@gmail.com>
2024-12-15 13:45:30 +0100

    basic python operatons

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+number1 = 10
+number2 = 5
+
+number1 = float(input("enter number1: "))
+number2 = float(input("enter number2: "))
+
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+print(f"addtition of {number1} and {number2} is {addition}")
+print(f"subtraction of {number1} and {number2} is {subtraction}")
+print(f"multiplication of {number1} and {number2} is {multiplication}")
